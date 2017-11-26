11dfc50cc529c3f34d7c00ba724053b43e0e2a7a
Jan Forst <ensonador@gmail.com>
2017-11-26 18:09:04 +0100

    cache rename

--- gulp/lib/cache.js
+++ gulp/lib/cache.js
@@ -2,4 +2,4 @@ require('events').defaultMaxListeners = 9999
 const Keyv = require('keyv')
 const mkdirp = require('mkdirp')
 mkdirp.sync('.cache')
-export const cache = new Keyv('sqlite://.cache/http.sqlite')
+export const cache = new Keyv('sqlite://.cache/http.db')
