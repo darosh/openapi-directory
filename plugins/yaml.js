c2477a5c84258330a993a0bc5a4def5ea329978c
Jan Forst <ensonador@gmail.com>
2017-12-01 10:28:54 +0100

    parametrize yaml plugin

--- plugins/yaml.js
+++ plugins/yaml.js
@@ -1,22 +1,25 @@
 const {obj} = require('through2')
-const {log, colors} = require('gulp-util')
+const {colors} = require('gulp-util')
+const glog = require('gulplog')
 const {dirname} = require('path')
 
 const PLUGIN_NAME = 'yaml'
 
-export function yaml ({verbose} = {}) {
+export function yaml (field) {
   const {dump} = require('js-yaml')
 
   return obj(function (file, enc, cb) {
-    if (file.yaml) {
-      file.contents = file.yaml
+    if (field) {
+      file.contents = Buffer.from(dump(file[field]))
     } else {
-      file.contents = Buffer.from(dump(file.json))
+      if (file.yaml) {
+        file.contents = file.yaml
+      } else {
+        file.contents = Buffer.from(dump(file.json))
+      }
     }
 
-    if (verbose) {
-      log(PLUGIN_NAME, colors.grey(dirname(file.relative)))
-    }
+    glog.debug(PLUGIN_NAME, colors.grey(dirname(file.relative)))
 
     cb(null, file)
   })
